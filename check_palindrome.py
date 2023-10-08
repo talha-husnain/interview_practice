f9064b1aad23883fffc14906bab4052f63acba64
Talha Husnain <24100245@lums.edu.pk>
2023-10-08 11:00:44 -0700

    A better solution

--- check_palindrome.py
+++ check_palindrome.py
@@ -18,5 +18,20 @@
 #             else:
 #                 return False
 #         return True
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # Convert all characters to lowercase and filter out non-alphanumeric characters
+        alphanumeric_chars = [char.lower() for char in s if char.isalnum()]
+        
+        # Join the alphanumeric characters back into a string
+        cleaned_string = ''.join(alphanumeric_chars)
+
+        # If the cleaned string is empty or is equal to its reverse, then it's a palindrome
+        return cleaned_string == cleaned_string[::-1]
+
 
         
