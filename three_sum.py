f3c5d22638c05c52e4ef4746ed8c93fffb04a0c1
Talha Husnain <24100245@lums.edu.pk>
2023-10-10 11:12:41 -0700

    3 sum

---
+++ three_sum.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        n = len(nums)
+        result = set()
+        
+        for i in range(n):
+            # Avoid processing duplicates
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            # Two pointers approach
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    result.add((nums[i], nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return list(result)
