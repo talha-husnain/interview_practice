54ab3744ead4c734cee9e6d4933047b2ebfcfecd
Talha Husnain <24100245@lums.edu.pk>
2023-10-18 17:38:43 -0700

    updated

--- 2_anagram_check.py
+++ 2_anagram_check.py
@@ -5,14 +5,15 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        first_length  = len(s)
+        first_length = len(s)
         second_length = len(t)
-        if first_length== second_length:
+        if first_length == second_length:
             sort_first = sorted(s)
             sort_second = sorted(t)
-            if  sort_first == sort_second:
+            if sort_first == sort_second:
                 return True
         return False
 
 
-        
+def are_anagrams(s1: str, s2: str) -> bool:
+    return sorted(s1) == sorted(s2)
