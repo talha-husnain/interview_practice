9e3d016b48cd00f135a6fd7d650540e193df94ca
Talha Husnain <24100245@lums.edu.pk>
2023-10-18 17:43:52 -0700

    vovel and consonent count

---
+++ 15_vovels_consonet.py
@@ -0,0 +1,18 @@
+def count_vowels_consonants(s: str) -> tuple:
+    vowels_count = 0
+    consonants_count = 0
+    vowels = "aeiouAEIOU"
+    for char in s:
+        if char.isalpha():
+            if char in vowels:
+                vowels_count += 1
+            else:
+                consonants_count += 1
+    return vowels_count, consonants_count
+
+
+def count_vowels_consonants(s: str) -> tuple:
+    vowels = [char for char in s if char.lower() in 'aeiou' and char.isalpha()]
+    consonants = [char for char in s if char.lower(
+    ) not in 'aeiou' and char.isalpha()]
+    return len(vowels), len(consonants)
