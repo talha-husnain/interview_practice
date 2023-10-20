b336c6299fb76a5640d5495568f3e2a7ba8d11dd
Talha Husnain <24100245@lums.edu.pk>
2023-10-20 15:07:16 -0700

    updated

--- 15_vovels_consonet.py
+++ 15_vovels_consonet.py
@@ -11,9 +11,6 @@ def count_vowels_consonants(s: str) -> tuple:
     return vowels_count, consonants_count
 
 
-
-
-    
 def count_vowels_consonants(s: str) -> tuple:
     vowels = [char for char in s if char.lower() in 'aeiou' and char.isalpha()]
     consonants = [char for char in s if char.lower(
