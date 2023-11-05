64ae1f4685dd626b8ff12f25dc3b11797e2d3c29
Talha Husnain <24100245@lums.edu.pk>
2023-11-04 19:29:10 -0700

    Substring

--- 17_substring.py
+++ 17_substring.py
@@ -16,3 +16,28 @@ class Solution(object):
             max_length = max(max_length, right - left + 1)
 
         return max_length
+
+# My way
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        max_num = 1
+        li = []
+        if len(s) == 0:
+            max_num = 0
+        for i in range(len(s)-2):
+
+            if s[i] != s[i+1] and s[i] not in li:
+                li.append(s[i])
+                count += 1
+                max_num = max(max_num, count)
+            else:
+                count = 1
+
+        return max_num
