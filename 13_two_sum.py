34b7a2a981c5ef4ee6b0dd6cfa1c95dcfaaf1a72
Talha Husnain <24100245@lums.edu.pk>
2023-11-13 13:24:24 +0500

    2sum 2pointers

--- 13_two_sum.py
+++ 13_two_sum.py
@@ -6,11 +6,27 @@ class Solution(object):
         :rtype: List[int]
         """
         check_set = {}
-        for i,num in enumerate(nums):
+        for i, num in enumerate(nums):
             req = target-num
             if req in check_set:
-                return [check_set[req],i]
-            check_set[num] =i
+                return [check_set[req], i]
+            check_set[num] = i
 
 
-        
+# def twoSum(nums, target):
+#     length = len(nums)
+#     left = 0
+#     right = length-1
+#     nums_arr = sorted(nums)
+#     while left < right:
+#         c_s = nums_arr[left]+nums_arr[right]
+#         if (c_s == target):
+#             return (nums[left], nums[right])
+#         elif (c_s > target):
+#             right -= 1
+#         elif (c_s < target):
+#             left += 1
+
+
+# result = twoSum([-1, -2, -3, 4, 5, 6, 7, 8], 9)
+# print(result)
