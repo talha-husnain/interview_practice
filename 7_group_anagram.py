d4b48fe65fe63b04bcf88d5742c3c1de5be5b835
Talha Husnain <24100245@lums.edu.pk>
2024-01-02 20:03:27 +0500

    Pusin all code

--- 7_group_anagram.py
+++ 7_group_anagram.py
@@ -1,18 +1,18 @@
 def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        dic = {}
-        for word in strs:
-            # Sort the characters in the word to create a key
-            sorted_word = ''.join(sorted(word))
-            
-            # Check if the sorted word is already in the dictionary, if not, add it as a key
-            if sorted_word not in dic:
-                dic[sorted_word] = [word]
-            else:
-                dic[sorted_word].append(word)
-        
-        # Return the values of the dictionary as the grouped anagrams
-        return list(dic.values())
+    """
+    :type strs: List[str]
+    :rtype: List[List[str]]
+    """
+    dic = {}
+    for word in strs:
+        # Sort the characters in the word to create a key
+        sorted_word = ''.join(sorted(word))
+
+        # Check if the sorted word is already in the dictionary, if not, add it as a key
+        if sorted_word not in dic:
+            dic[sorted_word] = [word]
+        else:
+            dic[sorted_word].append(word)
+
+    # Return the values of the dictionary as the grouped anagrams
+    return list(dic.values())
