edad430f5c61343835a84fecf09ade44f714ab53
Talha Husnain <24100245@lums.edu.pk>
2024-01-26 11:21:17 +0500

    Refactor

---
+++ arbisoft/aliyan_convo.py
@@ -0,0 +1,38 @@
+def encode_word(word):
+    # 6x5 matrix of the alphabet
+    alphabet = [list("ABCDE"), list("FGHIJ"), list("KLMNO"), list(
+        "PQRST"), list("UVWXY"), ["Z", '', '', '', '']]
+    instructions = ""
+    current_position = (0, 0)
+
+    for letter in word:
+        # Find the position of the letter in the matrix
+        position = [(index, row.index(letter))
+                    for index, row in enumerate(alphabet) if letter in row][0]
+
+        # Vertical movement
+        vertical_movement = position[0] - current_position[0]
+        if vertical_movement > 0:
+            instructions += 'd' * vertical_movement
+        elif vertical_movement < 0:
+            instructions += 'u' * abs(vertical_movement)
+
+        # Horizontal movement
+        horizontal_movement = position[1] - current_position[1]
+        if horizontal_movement > 0:
+            instructions += 'r' * horizontal_movement
+        elif horizontal_movement < 0:
+            instructions += 'l' * abs(horizontal_movement)
+
+        # Select the letter
+        instructions += '#'
+
+        # Update current position
+        current_position = position
+
+    return instructions
+
+
+# Test the function
+print(encode_word("UP"))
+print(encode_word("UPYOUGO"))
